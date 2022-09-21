ae3904dd8f46d589ba675d05818318132e09345d
o7q <jev.o7q@gmail.com>
2022-09-20 19:19:09 -0500

    Create version automation script

---
+++ assets/scripts.js
@@ -0,0 +1,12 @@
+function init()
+{
+    dlConfig();
+}
+
+function dlConfig()
+{
+    var ver = "v3.5.1";
+
+    document.getElementById("dlID").href ="https://github.com/o7q/MediaDownloader/releases/download/" + ver + "/MediaDownloader." + ver + ".redists-included.7z";
+    document.getElementById("dlID").innerHTML = "Download " + ver;
+}
