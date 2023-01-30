5db619ea85916a953208b986c430c0112d07c334
o7q <jev.o7q@gmail.com>
2023-01-29 19:33:09 -0600

    Update pictures

--- assets/pages/index/files/scripts.js
+++ assets/pages/index/files/scripts.js
@@ -1,6 +1,6 @@
 function bodyInit()
 {
-    var ver = "v3.8.2.0";
+    var ver = "v3.8.3.0";
 
     document.getElementById("dlID").href = "https://github.com/o7q/MediaDownloader/releases/download/" + ver + "/MediaDownloader." + ver + ".zip";
     document.getElementById("dlID").innerHTML = "Download " + ver;
