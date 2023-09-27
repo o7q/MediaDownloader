d06f6164fb05a5e8347b8f1b9759a29d2d721141
o7q <jev.o7q@gmail.com>
2023-09-26 20:12:58 -0500

    Update to-do

--- src/MediaDownloader/MediaDownloader/Scripts/Global.cs
+++ src/MediaDownloader/MediaDownloader/Scripts/Global.cs
@@ -4,8 +4,8 @@ namespace MediaDownloader
 {
     public static class Global
     {
-        public const string VERSION = "v4.2.3";
-        public const string VERSION_INTERNAL = "v4.2.3.0";
+        public const string VERSION = "v4.3.0";
+        public const string VERSION_INTERNAL = "v4.3.0.0";
         public static string VERSION_REMOTE;
 
         public static ConfigBase CONFIG = new ConfigBase();
